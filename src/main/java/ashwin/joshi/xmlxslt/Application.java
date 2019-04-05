009f9251d8cafa4ccb454c13fb2068a447c75bd3
Ashwin Joshi <ashwin.joshi12@live.com>
2019-04-05 15:21:06 -0400

    updated Main method;                                  set mongodb connection properties
    Signed-off-by: Ashwin Joshi <ashwin.joshi12@live.com>

--- src/main/java/ashwin/joshi/xmlxslt/Application.java
+++ src/main/java/ashwin/joshi/xmlxslt/Application.java
@@ -1,8 +1,5 @@
 package ashwin.joshi.xmlxslt;
 
-import ashwin.joshi.xmlxslt.model.BaseDocument;
-import ashwin.joshi.xmlxslt.model.PdfDocument;
-import ashwin.joshi.xmlxslt.model.XmlDocument;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,8 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-		PdfDocument pdf = new PdfDocument();
+		SpringApplication app = new SpringApplication(Application.class);
+		app.run();
 	}
 
 }
