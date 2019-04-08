b29e25f5c6a5f4fd3b8993afe6d2eff2607dbe99
Ashwin Joshi <ashwin.joshi12@live.com>
2019-04-08 13:02:48 -0400

    added command line runner bean for initializing a service
    Signed-off-by: Ashwin Joshi <ashwin.joshi12@live.com>

--- src/main/java/ashwin/joshi/xmlxslt/Application.java
+++ src/main/java/ashwin/joshi/xmlxslt/Application.java
@@ -1,14 +1,28 @@
 package ashwin.joshi.xmlxslt;
 
+import ashwin.joshi.xmlxslt.config.StorageProperties;
+import ashwin.joshi.xmlxslt.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(Application.class);
 		app.run();
+	}
+
+	@Bean
+	CommandLineRunner init(StorageService service){
+		return (args) -> {
+			service.deleteAll();
+			service.init();
+		};
 	}
 
 }
